da1d4faff8071d2379cc077256ab1ca906d5108f
Sanjih <48612771+Sanjih@users.noreply.github.com>
2025-07-21 14:13:18 +0300

    Update set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -2,8 +2,8 @@ import os
 import asyncio
 from telegram.ext import Application
 
-TOKEN = "8092994458:AAHIlUdlfh2E06VaXy6826Db0KH4KAstn6E" 
-WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://telegram-bot-nk3n.onrender.com/webhook")
+TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "8092994458:AAHI1Ud1fh2E06VaXy6826Db0KH4KAstn6E") 
+WEBHOOK_URL = os.getenv("WEBHOOK_URL", "https://telegram-bot-nk3n.onrender.com/webhook") # Tokony ho hita ao amin'ny Render io URL io
 
 async def main():
     application = (
@@ -12,8 +12,8 @@ async def main():
         .build()
     )
 
-    # initialize-na eto koa
-    await application.initialize() # <--- ATAOVY AZO ANTOKA FA await-ina ity
+    # initialize-na eto koa, satria Build Command ity
+    await application.initialize() 
 
     print(f"Setting webhook to: {WEBHOOK_URL}")
     try:
@@ -21,9 +21,8 @@ async def main():
         print("Webhook set successfully!")
     except Exception as e:
         print(f"Error setting webhook: {e}")
-        # Azo ampiana code fanampiny eto hijerena ny webhook_info raha ilaina
-        # webhook_info = await application.bot.get_webhook_info()
-        # print(f"Current Webhook Info: {webhook_info}")
+        # Mety ilaina ny mamoaka exception eto raha diso ny fametrahana webhook
+        # raise e 
 
 if __name__ == "__main__":
     asyncio.run(main())
